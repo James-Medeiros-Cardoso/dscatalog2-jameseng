c2c7c78e019df88dc47921a0ff707aaa3d4885c5
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-03-19 13:53:16 -0300

    Custon validation response

--- backend/src/main/java/com/jameseng/dscatalog2/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/jameseng/dscatalog2/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -47,6 +49,31 @@ public class ResourceExceptionHandler {
 		err.setError("Database Exception");
 		err.setMessage(e.getMessage()); // getMessage() = pega a mensagem definida no service
 		err.setPath(request.getRequestURI()); // path da requisição
+
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class) // MethodArgumentNotValidException = será interceptada
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		// HttpServletRequest = tem informações sobre a requisição
+
+		// UNPROCESSABLE_ENTITY = entidade não processável (422)
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+		ValidationError err = new ValidationError();
+
+		err.setTimestamp(Instant.now()); // Instant.now() = pega o horário atual
+		err.setStatus(status.value()); // código 422
+		err.setError("Validation Exception");
+		err.setMessage(e.getMessage()); // getMessage() = pega a mensagem definida no service
+		err.setPath(request.getRequestURI()); // path da requisição
+		
+		// getBindingResult() = coleta os erros da validação
+		// getFieldErrors() = lista de erros da validação
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(status).body(err);
 	}
