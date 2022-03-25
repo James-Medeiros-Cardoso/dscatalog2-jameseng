971159f9d9ea583af00bd9d35c06f4e3c1099989
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-03-25 20:03:16 -0300

    H2 Security Configuration

--- backend/src/main/java/com/jameseng/dscatalog2/config/ResourceServerConfig.java
+++ backend/src/main/java/com/jameseng/dscatalog2/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.jameseng.dscatalog2.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,7 +19,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token" }; // essa rota tem que ser publico para fazer o login
+	@Autowired
+	private Environment env;
+
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" }; // essa rota tem que ser publico para
+																				// fazer o login
 
 	// rotas liberadas para operador e admin:
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
@@ -32,9 +39,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+
+		// liberar o H2:
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
 		http.authorizeRequests().antMatchers(PUBLIC).permitAll() // liberado de autorizações / permitAll() = permite
 																	// todos
-
 				// HttpMethod.GET = libera o GET no vetor OPERATOR_OR_ADMIN
 				// e .permitAll() = libera para todos
 				.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
