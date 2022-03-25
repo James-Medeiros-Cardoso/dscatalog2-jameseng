ba5f36a9e85f8a721509f160b7cc2333f8d66bde
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-03-25 19:25:56 -0300

    Resource Server

---
+++ backend/src/main/java/com/jameseng/dscatalog2/config/ResourceServerConfig.java
@@ -0,0 +1,52 @@
+package com.jameseng.dscatalog2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer // essa classe implementa p ResourceServer do oauth2
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private JwtTokenStore tokenStore;
+
+	private static final String[] PUBLIC = { "/oauth/token" }; // essa rota tem que ser publico para fazer o login
+
+	// rotas liberadas para operador e admin:
+	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+
+	// rotas somente para admin:
+	private static final String[] ADMIN = { "/users/**" };
+
+	// o resource server será capaz de analisar o token recebido
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll() // liberado de autorizações / permitAll() = permite
+																	// todos
+
+				// HttpMethod.GET = libera o GET no vetor OPERATOR_OR_ADMIN
+				// e .permitAll() = libera para todos
+				.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+
+				// quem for "OPERATOR" ou "ADMIN" acessa as rotas do OPERATOR_OR_ADMIN
+				.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+
+				// quem "ADMIN" acessa as rotas do ADMIN
+				.antMatchers(ADMIN).hasAnyRole("ADMIN")
+
+				// quem acessar qualquer outra rota, tem que estar logado.
+				.anyRequest().authenticated();
+	}
+
+}
